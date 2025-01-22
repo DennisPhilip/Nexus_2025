cd8345ecb23ba7b3de12a70ebc704324b7dd3a7c
DennisPhilip <dennisphilip.gg@gmail.com>
2025-01-23 00:34:36 +0530

    fixed errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,5 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import dynamic from "next/dynamic";
 
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+const RootLayout = dynamic(() => import("./rootLayout"), { ssr: false });
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "NEXUS PES",
-  description: "Official Website for NEXUS PES TECH CLUB IN PES UNIVERSITY BANGALORE",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/nxs.png" sizes="any" />
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+export default RootLayout;
